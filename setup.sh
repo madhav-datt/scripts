65c2f44ee7cf792c31d9b784c020cd0921de26e0
madhav-datt <madhav.datt@hotmail.com>
2016-05-10 21:27:47 +0530

    add install packages to setup script

--- setup.sh
+++ setup.sh
@@ -4,9 +4,17 @@
 # Copyright (C)   2016    Madhav Datt
 #
 
-#apt-get update
-#apt-get upgrade
+add-apt-repository ppa:mystic-mirage/pycharm
+apt-get update
+apt-get upgrade
 
-list_of_packages="python atom g++ gcc cython nautilus rsync dpkg git"
+list_of_packages="python atom g++ gcc cython nautilus rsync pip dpkg git java terminator pycharm-community mysql-server"
 
 apt-get --ignore-missing install $list_of_packages
+service mysql start
+
+python_packages="numpy scipy sympy networkx matplotlib nose nltk requests"
+
+pip install $python_packages
+
+# Manually install eclipse and pycharm
